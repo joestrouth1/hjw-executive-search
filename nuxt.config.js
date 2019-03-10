663ad46b7d40f6f98bd2c676428472b9f7136615
Joe Strouth <joestrouth1@gmail.com>
2019-03-10 17:47:42 -0500

    icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,7 @@ module.exports = {
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
-    '@nuxtjs/pwa'
+    ['@nuxtjs/pwa']
   ],
   /*
   ** Axios module configuration
